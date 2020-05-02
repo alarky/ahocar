be520f2d97b32c319fa362ddeaf99a107291681f
alarky <alarky@gmail.com>
2020-05-03 07:02:02 +0900

    fix dualize

--- fisheye2equirectangular/dualize_fisheye.py
+++ fisheye2equirectangular/dualize_fisheye.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as np
 import sys
 
 image = cv2.imread(sys.argv[1])
 
-rimage = cv2.flip(image, 1)
+rimage = np.zeros(image.shape, dtype='uint8')
 
 nimage = cv2.hconcat([image, rimage])
 
